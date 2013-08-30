5c12bf4938767b064ed4ee32c360ade94a8c8b51
Pak Youngrok <pak.youngrok@gmail.com>
2013-08-30 11:13:16 +0900

    imitate fancy.com

--- shopping/views.py
+++ shopping/views.py
@@ -1,8 +1,4 @@
-from django.http.response import HttpResponse
 from django.shortcuts import render_to_response
 
 def index(request):
-	return HttpResponse('Hello World')
-
-def hello(request):
 	return render_to_response('index.html', locals())
